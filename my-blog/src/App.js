1a4a328dca743e9a8c2508c44d681fa58230ab1a
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-01-21 22:31:09 -0600

    Error page added

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -5,22 +5,22 @@ import NavBar from './components/navBar';
 import About from './components/about';
 import SignupForm from './components/signupForm';
 import Articles from './components/articles';
+import Error from './components/error';
 
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
-    <>
-      <Router>
-          <NavBar />
-          <Routes>
-            <Route path='/' element={<Home />}/>
-            <Route path='/articles' element={<Articles />}/>
-            <Route path='/about' element={<About />}/>
-            <Route path='/signupform' element={<SignupForm />}/>
-          </Routes>
-      </Router>
-    </>
+    <Router>
+        <NavBar />
+        <Routes>
+          <Route path='/' element={<Home />}/>
+          <Route path='/articles' element={<Articles />}/>
+          <Route path='/about' element={<About />}/>
+          <Route path='/signupform' element={<SignupForm />}/>
+          <Route path='*' element={<Error />} />
+        </Routes>
+    </Router>
   );
 }
 
---
+++ my-blog/src/components/error.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      <h3>Page Not Found</h3>
+    </div>
+  )
+}
+
+export default Error;
