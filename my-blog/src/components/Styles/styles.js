d4b36ee5f2f07f4323f346336ca60d3b936c439b
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-01-24 22:40:47 -0600

    applied css using styled components to home page

---
+++ my-blog/src/components/Styles/styles.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components'
+
+const LinkHeading = styled.h1`
+  font-size: 2rem;
+  color: purple
+`
+
+export { LinkHeading };
--- my-blog/src/components/home/home.jsx
+++ my-blog/src/components/home/home.jsx
@@ -1,9 +1,10 @@
-import React from 'react'
+import React from 'react';
+import { LinkHeading } from '../../components/Styles/styles'
 
 const Home = () => {
   return (
     <div>
-      <h1>This is Home</h1>
+      <LinkHeading>This is Home</LinkHeading>
     </div>
   )
 }
