d252a2cd3e6a860bb65cbdb8313ac6cd2d2097bc
Yachao Fan <61946174+ycfan23@users.noreply.github.com>
2022-02-02 10:42:30 -0600

    installed axios and made GET request

--- my-blog/src/App.js
+++ my-blog/src/App.js
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 import Home from './components/home/home';
 import NavBar from './components/navBar/navBar';
@@ -10,12 +11,20 @@ import Error from './components/error/error';
 import { BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 
 const App = () => {
+  const [articles, setArticles] = useState([]);
+
+  useEffect(() => {
+    axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then(res => setArticles(res.data))
+  }, [])
+
+
   return (
     <Router>
         <NavBar />
         <Routes>
           <Route path='/' element={<Home />}/>
-          <Route path='/articles' element={<Articles />}/>
+          <Route path='/articles' element={<Articles articles={articles} />}/>
           <Route path='/about' element={<About />}/>
           <Route path='/signupform' element={<SignupForm />}/>
           <Route path='*' element={<Error />} />
